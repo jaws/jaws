8850d639c8f38f6a1f3f3153dc60408c08d4ddd9
Ajay Saini <ajcse1@gmail.com>
2017-12-26 10:26:36 -0800

    Networks have individual functions in their files with consistent names. jaws.py contains main function

--- source/gcnet2nc.py
+++ source/gcnet2nc.py
@@ -1,9 +1,8 @@
-import argparse
 from netCDF4 import Dataset
 from datetime import date
 
 
-def ascii2nc(f):
+def gcnet2nc(f):
 
 	# NC file setup
 	global op_file
@@ -515,24 +514,3 @@ def ascii2nc(f):
 	    m += 1
 
 	root_grp.close()
-
-
-
-
-
-def Main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("input", help="The GCNET file you wish to convert to netCDF.", type=str)
-    parser.add_argument("row_count", help="Total number of rows in your dataset", type=int)
-    parser.add_argument("-o", "--output", help="Path where you want to store the output file", type=str)
-
-    global args
-    
-    args = parser.parse_args()
-
-    ascii2nc(args.input)
-    
-    print "The file " + str(args.input) + " is converted into netCDF format and is saved at " + op_file
-
-if __name__ == '__main__':
-    Main()
--- source/promice2nc.py
+++ source/promice2nc.py
@@ -1,4 +1,3 @@
-import argparse
 from netCDF4 import Dataset
 from datetime import date
 from astropy.io import ascii
@@ -304,22 +303,3 @@ def promice2nc(f):
 
 
 	root_grp.close()
-
-
-
-def Main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("input", help="The PROMICE file you wish to convert to netCDF.", type=str)
-    parser.add_argument("row_count", help="Total number of rows in your dataset", type=int)
-    parser.add_argument("-o", "--output", help="Path where you want to store the output file", type=str)
-
-    global args
-    
-    args = parser.parse_args()
-
-    promice2nc(args.input)
-    
-    print "The file " + str(args.input) + " is converted into netCDF format and is saved at " + op_file
-
-if __name__ == '__main__':
-    Main()
