ad27e106f830d093866cc530f27d7ab7c19fbc25
Ajay Saini <ajcse1@gmail.com>
2018-02-22 19:33:56 -0800

    Analysis - print meaningful error messages and restructured code

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import argparse
+import sys
 
 mpl.rc('figure', figsize = (15, 10))
 mpl.rc('font', size = 12)
@@ -26,80 +27,107 @@ if args.year:
 if args.month:
     df = df[df.month == args.month]
 
+if df.size == 0:
+	print('ERROR: Provide a valid year and month')
+	parser.print_help()
+	sys.exit(1)
+
+def check_error(var_x, var_y):
+	if len(var_x) != len(var_y):
+		if var_y == hours:
+			print('ERROR: Provide data for each hour of the day')
+		elif var_y == days:
+			print('ERROR: Provide data for each day of the month')
+		elif var_y == days_year:
+			print('ERROR: Provide data for each day of the year')
+		elif var_y == months:
+			print('ERROR: Provide data for all the months')
+		
+		parser.print_help()
+		sys.exit(1)
 
 year = df['year']
 
 df[args.var].replace([999.00], [245], inplace=True)
 
-def month_name():
-	global month, days
+global month, days
 
-	df['month'] = df['month'].astype(str)
-	month = df['month']
-	if month[0][0] == '1':
-	    month[0][0] = 'Jan'
-	    days = range(1,32)
-	elif month[0][0] == '2':
-	    month[0][0] = 'Feb'
-	    days = range(1,29)
-	elif month[0][0] == '3':
-	    month[0][0] = 'Mar'
-	    days = range(1,32)
-	elif month[0][0] == '4':
-	    month[0][0] = 'Apr'
-	    days = range(1,31)
-	elif month[0][0] == '5':
-	    month[0][0] = 'May'
-	    days = range(1,32)
-	elif month[0][0] == '6':
-	    month[0][0] = 'Jun'
-	    days = range(1,31)
-	elif month[0][0] == '7':
-	    month[0][0] = 'Jul'
-	    days = range(1,32)
-	elif month[0][0] == '8':
-	    month[0][0] = 'Aug'
-	    days = range(1,32)
-	elif month[0][0] == '9':
-	    month[0][0] = 'Sep'
-	    days = range(1,31)
-	elif month[0][0] == '10':
-	    month[0][0] = 'Oct'
-	    days = range(1,32)
-	elif month[0][0] == '11':
-	    month[0][0] = 'Nov'
-	    days = range(1,31)
-	elif month[0][0] == '12':
-	    month[0][0] = 'Dec'
-	    days = range(1,32)
+df['month'] = df['month'].astype(str)
+month = df['month']
+if month[0][0] == '1':
+	month[0][0] = 'Jan'
+	days = range(1,32)
+elif month[0][0] == '2':
+	month[0][0] = 'Feb'
+	days = range(1,29)
+elif month[0][0] == '3':
+	month[0][0] = 'Mar'
+	days = range(1,32)
+elif month[0][0] == '4':
+	month[0][0] = 'Apr'
+	days = range(1,31)
+elif month[0][0] == '5':
+	month[0][0] = 'May'
+	days = range(1,32)
+elif month[0][0] == '6':
+	month[0][0] = 'Jun'
+	days = range(1,31)
+elif month[0][0] == '7':
+	month[0][0] = 'Jul'
+	days = range(1,32)
+elif month[0][0] == '8':
+	month[0][0] = 'Aug'
+	days = range(1,32)
+elif month[0][0] == '9':
+	month[0][0] = 'Sep'
+	days = range(1,31)
+elif month[0][0] == '10':
+	month[0][0] = 'Oct'
+	days = range(1,32)
+elif month[0][0] == '11':
+	month[0][0] = 'Nov'
+	days = range(1,31)
+elif month[0][0] == '12':
+	month[0][0] = 'Dec'
+	days = range(1,32)
 
-	return month, days
 
+global hours, days_year, months
+
+hours = range(1,25)
+
+if year[0][0]%4 == 0:
+	days_year = range(1,367)
+else:
+	days_year = range(1,366)
+
+months = range(1,13)
 
 def diurnal():
-	global var_hour_avg, var_hour_sd, hours
+	global var_hour_avg, var_hour_sd
 	
-	month_name()
 	hour = df['hour']
 	var_hour_avg = df[args.var].groupby(hour).mean()
 	var_hour_sd = df[args.var].groupby(hour).std()
-	hours = range(1,25)
+
+	check_error(var_hour_avg, hours)
 
 	return var_hour_avg, var_hour_sd, hours
 
 def monthly():
 	global var_day_avg, var_day_max, var_day_min
 	
-	month_name()
 	day = df['day']
 	var_day_avg = df[args.var].groupby(day).mean()
 	var_day_max = df[args.var].groupby(day).max()
 	var_day_min = df[args.var].groupby(day).min()
 
+	check_error(var_day_avg, days)
+
 	return var_day_avg, var_day_max, var_day_min
 
 def annual():
-	global var_jdt_avg, var_jdt_max, var_jdt_min, days_year
+	global var_jdt_avg, var_jdt_max, var_jdt_min
 	
 	df['julian_decimal_time'] = df['julian_decimal_time'].astype(int)
 	jdt = df['julian_decimal_time']
@@ -107,20 +135,18 @@ def annual():
 	var_jdt_max = df[args.var].groupby(jdt).max()
 	var_jdt_min = df[args.var].groupby(jdt).min()
 
-	if year[0][0]%4 == 0:
-		days_year = range(1,367)
-	else:
-		days_year = range(1,366)
+	check_error(var_jdt_avg, days_year)
 
 	return var_jdt_avg, var_jdt_max, var_jdt_min, days_year
 
 def seasonal():
-	global var_month_avg, var_month_sd, months
+	global var_month_avg, var_month_sd
 
 	month = df['month']
 	var_month_avg = df[args.var].groupby(month).mean()
 	var_month_sd = df[args.var].groupby(month).std()
-	months = range(1,13)
+
+	check_error(var_month_avg, months)
 
 	return var_month_avg, var_month_sd, months
 
