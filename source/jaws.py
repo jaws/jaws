ae451ffa86e846e7dc91e6ab75b42ab3a3760a1a
Ajay Saini <ajcse1@gmail.com>
2018-03-09 20:00:56 -0800

    lazy commit

--- source/jaws.py
+++ source/jaws.py
@@ -5,105 +5,105 @@ from collections import OrderedDict
 from datetime import datetime
 
 def get_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "input_file", nargs='?',
-        help="Raw L2 data file to convert to netCDF (or use -i option)",
-        type=str)
-    parser.add_argument(
-        "output_file", nargs='?',
-        help="Output netCDF file (or use -o option)", type=str)
-    parser.add_argument(
-        "-i", "--fl_in", "--input",
-        help=("Raw L2 data file to convert to netCDF "
-              "(or use first positional argument)"),
-        type=str)
-    parser.add_argument(
-        "-o", "--fl_out", "--output",
-        help="Output netCDF file (or use last positional argument)",
-        type=str)
-    parser.add_argument(
-        "-3", "--format3", "--3", "--fl_fmt=classic",
-        help="Output file in netCDF3 CLASSIC (32-bit offset) storage format",
-        action="store_true")
-    parser.add_argument(
-        "-4", "--format4", "--4", "--netcdf4",
-        help="Output file in netCDF4 (HDF5) storage format",
-        action="store_true")
-    parser.add_argument(
-        "-5", "--format5", "--5", "--fl_fmt=64bit_data",
-        help=("Output file in netCDF3 64-bit data"
-              " (i.e., CDF5, PnetCDF) storage format"),
-        action="store_true")
-    parser.add_argument(
-        "-6", "--format6", "--6", "--64",
-        "--fl_fmt=64bit_offset",
-        help="Output file in netCDF3 64-bit offset storage format",
-        action="store_true")
-    parser.add_argument(
-        "-7", "--format7", "--7", "--fl_fmt=netcdf4_classic",
-        help="Output file in netCDF4 CLASSIC format (3+4=7)",
-        action="store_true")
-    parser.add_argument(
-        "-d", "--derive_times",
-        help=("For GCNet, extra variables ('month', 'day' and 'hour')"
-              " will be derived for further analysis. It will take more time"),
-        action="store_true")
-    parser.add_argument(
-        "-s", "--station_name",
-        help="Override default station name",
-        type=str)
-    parser.add_argument(
-        "-t", "--timezone",
-        help="Change the timezone, default is UTC",
-        default='UTC', type=str)
-    parser.add_argument(
-        "-f", "--fillvalue_float",
-        help="Override default float _FillValue", type=float)
-    parser.add_argument(
-        "-D", "--dbg_lvl", "--debuglevel",
-        help="Debug-level is lvl", default=0, type=int)
-    
-    '''
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		"input_file", nargs='?',
+		help="Raw L2 data file to convert to netCDF (or use -i option)",
+		type=str)
+	parser.add_argument(
+		"output_file", nargs='?',
+		help="Output netCDF file (or use -o option)", type=str)
+	parser.add_argument(
+		"-i", "--fl_in", "--input",
+		help=("Raw L2 data file to convert to netCDF "
+			  "(or use first positional argument)"),
+		type=str)
+	parser.add_argument(
+		"-o", "--fl_out", "--output",
+		help="Output netCDF file (or use last positional argument)",
+		type=str)
+	parser.add_argument(
+		"-3", "--format3", "--3", "--fl_fmt=classic",
+		help="Output file in netCDF3 CLASSIC (32-bit offset) storage format",
+		action="store_true")
+	parser.add_argument(
+		"-4", "--format4", "--4", "--netcdf4",
+		help="Output file in netCDF4 (HDF5) storage format",
+		action="store_true")
+	parser.add_argument(
+		"-5", "--format5", "--5", "--fl_fmt=64bit_data",
+		help=("Output file in netCDF3 64-bit data"
+			  " (i.e., CDF5, PnetCDF) storage format"),
+		action="store_true")
+	parser.add_argument(
+		"-6", "--format6", "--6", "--64",
+		"--fl_fmt=64bit_offset",
+		help="Output file in netCDF3 64-bit offset storage format",
+		action="store_true")
+	parser.add_argument(
+		"-7", "--format7", "--7", "--fl_fmt=netcdf4_classic",
+		help="Output file in netCDF4 CLASSIC format (3+4=7)",
+		action="store_true")
+	parser.add_argument(
+		"-d", "--derive_times",
+		help=("For GCNet, extra variables ('month', 'day' and 'hour')"
+			  " will be derived for further analysis. It will take more time"),
+		action="store_true")
+	parser.add_argument(
+		"-s", "--station_name",
+		help="Override default station name",
+		type=str)
+	parser.add_argument(
+		"-t", "--timezone",
+		help="Change the timezone, default is UTC",
+		default='UTC', type=str)
+	parser.add_argument(
+		"-f", "--fillvalue_float",
+		help="Override default float _FillValue", type=float)
+	parser.add_argument(
+		"-D", "--dbg_lvl", "--debuglevel",
+		help="Debug-level is lvl", default=0, type=int)
+	
+	'''
 	
 	/* Argument --debuglevel in increasing levels of verbosity */
-    jaws_dbg_quiet, /* 0 */ Quiet all non-error messages
-    jaws_dbg_std,   /* 1 */ Elapsed time
-    jaws_dbg_fl,    /* 2 */ Input/Output Filepaths
-    jaws_dbg_coords,/* 3 */ lat, lon, stn_name
-    jaws_dbg_time,  /* 4 */ time and sza
-    jaws_dbg_var1,  /* 5 */ For GCNet- quality control variables, For PROMICE- lat_GPS, lon_GPS
-    jaws_dbg_var2,  /* 6 */ For GCNet- month and day, For PROMICE- ice_velocity
-    
-    '''
-    return parser
+	jaws_dbg_quiet, /* 0 */ Quiet all non-error messages
+	jaws_dbg_std,   /* 1 */ Elapsed time
+	jaws_dbg_fl,	/* 2 */ Input/Output Filepaths
+	jaws_dbg_coords,/* 3 */ lat, lon, stn_name
+	jaws_dbg_time,  /* 4 */ time and sza
+	jaws_dbg_var1,  /* 5 */ For GCNet- quality control variables, For PROMICE- lat_GPS, lon_GPS
+	jaws_dbg_var2,  /* 6 */ For GCNet- month and day, For PROMICE- ice_velocity
+	
+	'''
+	return parser
 
 
 def parse_args(args):
-    return get_parser().parse_args(args)
+	return get_parser().parse_args(args)
 
 
 def get_stations():
-    """Read stations.txt and parse it into an ordered dict."""
-    with open(relative_path('resources/stations.txt')) as stream:
-        stations = stream.read().split('\n')
+	"""Read stations.txt and parse it into an ordered dict."""
+	with open(relative_path('resources/stations.txt')) as stream:
+		stations = stream.read().split('\n')
 
-    # remove blank lines
-    stations = [i.strip() for i in stations if i.strip()]
-    ordered = collections.OrderedDict(blank=[])
+	# remove blank lines
+	stations = [i.strip() for i in stations if i.strip()]
+	ordered = collections.OrderedDict(blank=[])
 
-    errmsg = 'stations.txt is corrupted or malformed and could not be parsed.'
-    for station in stations:
-        match = re.match('(.+) +(-?[0-9.]+) + (-?[0-9.]+) *(.*)$', station)
-        if not match:
-            raise RuntimeError(errmsg)
-        name, lon, lat, name2 = match.groups()
-        lon, lat = float(lon), float(lat)
-        name2 = name2.strip()
-        value = [lon, lat, name2] if name2 else [lon, lat]
-        ordered[name.strip()] = value
+	errmsg = 'stations.txt is corrupted or malformed and could not be parsed.'
+	for station in stations:
+		match = re.match('(.+) +(-?[0-9.]+) + (-?[0-9.]+) *(.*)$', station)
+		if not match:
+			raise RuntimeError(errmsg)
+		name, lon, lat, name2 = match.groups()
+		lon, lat = float(lon), float(lat)
+		name2 = name2.strip()
+		value = [lon, lat, name2] if name2 else [lon, lat]
+		ordered[name.strip()] = value
 
-    return ordered
+	return ordered
 
 def Main():
 	start_time = datetime.now()
@@ -331,4 +331,4 @@ def Main():
 
 
 if __name__ == '__main__':
-    Main()
+	Main()
