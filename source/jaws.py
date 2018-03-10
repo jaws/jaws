28931e680a982361b48ac8ebd464a168a1a63ef1
Ajay Saini <ajcse1@gmail.com>
2018-03-09 18:30:14 -0800

    Created new function for argument parsing

--- source/jaws.py
+++ source/jaws.py
@@ -4,6 +4,84 @@ import gcnet2nc, promice2nc, aaws2nc
 from collections import OrderedDict
 from datetime import datetime
 
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "input_file", nargs='?',
+        help="Raw L2 data file to convert to netCDF (or use -i option)",
+        type=str)
+    parser.add_argument(
+        "output_file", nargs='?',
+        help="Output netCDF file (or use -o option)", type=str)
+    parser.add_argument(
+        "-i", "--fl_in", "--input",
+        help=("Raw L2 data file to convert to netCDF "
+              "(or use first positional argument)"),
+        type=str)
+    parser.add_argument(
+        "-o", "--fl_out", "--output",
+        help="Output netCDF file (or use last positional argument)",
+        type=str)
+    parser.add_argument(
+        "-3", "--format3", "--3", "--fl_fmt=classic",
+        help="Output file in netCDF3 CLASSIC (32-bit offset) storage format",
+        action="store_true")
+    parser.add_argument(
+        "-4", "--format4", "--4", "--netcdf4",
+        help="Output file in netCDF4 (HDF5) storage format",
+        action="store_true")
+    parser.add_argument(
+        "-5", "--format5", "--5", "--fl_fmt=64bit_data",
+        help=("Output file in netCDF3 64-bit data"
+              " (i.e., CDF5, PnetCDF) storage format"),
+        action="store_true")
+    parser.add_argument(
+        "-6", "--format6", "--6", "--64",
+        "--fl_fmt=64bit_offset",
+        help="Output file in netCDF3 64-bit offset storage format",
+        action="store_true")
+    parser.add_argument(
+        "-7", "--format7", "--7", "--fl_fmt=netcdf4_classic",
+        help="Output file in netCDF4 CLASSIC format (3+4=7)",
+        action="store_true")
+    parser.add_argument(
+        "-d", "--derive_times",
+        help=("For GCNet, extra variables ('month', 'day' and 'hour')"
+              " will be derived for further analysis. It will take more time"),
+        action="store_true")
+    parser.add_argument(
+        "-s", "--station_name",
+        help="Override default station name",
+        type=str)
+    parser.add_argument(
+        "-t", "--timezone",
+        help="Change the timezone, default is UTC",
+        default='UTC', type=str)
+    parser.add_argument(
+        "-f", "--fillvalue_float",
+        help="Override default float _FillValue", type=float)
+    parser.add_argument(
+        "-D", "--dbg_lvl", "--debuglevel",
+        help="Debug-level is lvl", default=0, type=int)
+        '''
+	    /* Argument --debuglevel in increasing levels of verbosity */
+        
+        jaws_dbg_quiet, /* 0 */ Quiet all non-error messages
+        jaws_dbg_std,   /* 1 */ Elapsed time
+        jaws_dbg_fl,    /* 2 */ Input/Output Filepaths
+        jaws_dbg_coords,/* 3 */ lat, lon, stn_name
+        jaws_dbg_time,  /* 4 */ time and sza
+        jaws_dbg_var1,  /* 5 */ For GCNet- quality control variables, For PROMICE- lat_GPS, lon_GPS
+        jaws_dbg_var2,  /* 6 */ For GCNet- month and day, For PROMICE- ice_velocity
+        
+        '''
+    return parser
+
+
+def parse_args(args):
+    return get_parser().parse_args(args)
+
+
 def Main():
 	start_time = datetime.now()
 	
