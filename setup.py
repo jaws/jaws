580f4967e7e8690451cb41bbe8ba648b03ef85ec
Ajay Saini <ajcse1@gmail.com>
2018-03-16 19:22:43 -0700

    lazy commit

--- setup.py
+++ setup.py
@@ -29,5 +29,5 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Operating System :: OS Independent"],
 
-    install_requires=['pandas', 'numpy', 'xarray', 'pytz', 'matplotlib', 'netCDF4']
+    install_requires=['pandas', 'numpy', 'xarray', 'pytz', 'matplotlib', 'netcdf4']
 )
