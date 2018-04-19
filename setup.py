ed347cd89164df868f39786fbf535c3244de267d
Ajay Saini <ajcse1@gmail.com>
2018-04-18 18:13:43 -0700

    Add sub-directories of resources to MANIFEST.in

--- setup.py
+++ setup.py
@@ -9,26 +9,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="jaws",
-    version='0.2.6',
+    version='0.3',
     description='Software to convert idiosyncratic ASCII formats to netCDF formats',
     url='https://github.com/jaws/jaws',
     author='Ajay Saini',
     author_email='ajcse1@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    #data_files=[('jaws/resources', ['jaws/resources/stations.txt']),
-    #			('jaws/resources/aaws', ['jaws/resources/aaws/columns.txt']),
-    #			('jaws/resources/aaws', ['jaws/resources/aaws/ds.json']),
-    #			('jaws/resources/aaws', ['jaws/resources/aaws/encoding.json']),
-    #			('jaws/resources/gcnet', ['jaws/resources/gcnet/columns.txt']),
-    #			('jaws/resources/gcnet', ['jaws/resources/gcnet/ds.json']),
-    #			('jaws/resources/gcnet', ['jaws/resources/gcnet/encoding.json']),
-    #			('jaws/resources/gcnet', ['jaws/resources/gcnet/quality_control.json']),
-    #			('jaws/resources/promice', ['jaws/resources/promice/aliases.txt']),
-    #			('jaws/resources/promice', ['jaws/resources/promice/columns.txt']),
-    #			('jaws/resources/promice', ['jaws/resources/promice/ds.json']),
-    #			('jaws/resources/promice', ['jaws/resources/promice/encoding.json'])],
-	platforms=["any"],
+    platforms=["any"],
     keywords=['xarray', 'netcdf', 'pandas', 'data', 'science', 'network', 'meteorology', 'climate', 'automated', 'weather', 'stations'],
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
