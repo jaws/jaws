803e1baa7dd991bcaf0529c228ee9a2fe0b43b78
Ajay Saini <ajcse1@gmail.com>
2018-05-02 16:03:18 -0700

    Add long_description in setup.py

--- setup.py
+++ setup.py
@@ -6,19 +6,30 @@ https://github.com/pypa/sampleproject
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name="jaws",
-    version='0.3',
+    version='0.3.4',
     description='Software to convert idiosyncratic ASCII formats to netCDF formats',
+    long_description=long_description,
     url='https://github.com/jaws/jaws',
     author='Ajay Saini',
     author_email='ajcse1@gmail.com',
+    license='Apache License, Version 2.0',
     packages=find_packages(),
     include_package_data=True,
     platforms=["any"],
     keywords=['xarray', 'netcdf', 'pandas', 'data', 'science', 'network', 'meteorology', 'climate', 'automated', 'weather', 'stations'],
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 2",
