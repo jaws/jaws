fsds_adjust.py: Correct function to calculate derivative

jaws/fsds_adjust.py
@@ -4,6 +4,7 @@ import requests
 
 import numpy as np
 import pandas as pd
+import scipy.interpolate
 import xarray as xr
 
 try:
@@ -16,6 +17,19 @@ def deg_to_rad(list_deg):
     list_rad = [np.radians(i) for i in list_deg]
 
     return list_rad
+
+
+def first_order_derivative(tm, var):
+    dv = scipy.interpolate.interp1d(tm, var, fill_value='extrapolate')
+    alpha = 0.01
+    dv_left, dv_right = ([None] * len(tm) for _ in range(2))
+    i = 0
+    for hour in tm:
+        dv_left[i] = float(dv(hour - alpha))
+        dv_right[i] = float(dv(hour + alpha))
+        i += 1
+    slope = [(x1 - x2) / (2 * alpha) for (x1, x2) in zip(dv_right, dv_left)]
+    return slope
 
 
 def post_process(df, dates, stn_name, sfx):
@@ -56,19 +70,21 @@ def post_process(df, dates, stn_name, sfx):
         albedo = [abs(i) for i in albedo]
         hours = np.arange(len(albedo))
 
-        dy = np.diff(albedo, 1)
+        '''dy = np.diff(albedo, 1)
         dx = np.diff(hours, 1)
         yfirst = dy/dx
         xfirst = 0.5 * (hours[:-1] + hours[1:])
 
         dyfirst = np.diff(yfirst, 1)
         dxfirst = np.diff(xfirst, 1)
-        ysecond = dyfirst / dxfirst
+        ysecond = dyfirst / dxfirst'''
+
+        alb_second_derv = first_order_derivative(hours, first_order_derivative(hours, albedo))
 
         idx = 0
-        while idx < len(ysecond):
+        while idx < len(alb_second_derv):
             try:
-                if ysecond[idx] > thrsh:
+                if abs(alb_second_derv[idx]) > thrsh:
                     fsds_jaws[idx] = common.fillvalue_float
                 if fsds_jaws[idx] < 0:
                     fsds_jaws[idx] = 0
@@ -93,9 +109,9 @@ def post_process(df, dates, stn_name, sfx):
 
             idx += 1
 
-    df['fsds_adjusted_new'] = pd.to_numeric(df['fsds_adjusted_new'], errors='coerce')
+    #df['fsds_adjusted_new'] = pd.to_numeric(df['fsds_adjusted_new'], errors='coerce')
     df['fsus_adjusted'] = pd.to_numeric(df['fsus_adjusted'], errors='coerce')
-    fsds_adjusted_values_new = df['fsds_adjusted_new'].tolist()
+    fsds_adjusted_values_new = df['fsds_adjusted'].tolist()
     fsus_adjusted_values = df['fsus_adjusted'].tolist()
 
     return fsds_adjusted_values_new, fsus_adjusted_values