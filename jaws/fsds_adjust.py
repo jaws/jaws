e341cc39bec5536345078f2de6d2a66a117a6623
Ajay Saini <ajcse1@gmail.com>
2019-03-30 14:17:33 -0700

    fsds_adjust.py: Correct values in post-process function

--- jaws/fsds_adjust.py
+++ jaws/fsds_adjust.py
@@ -100,7 +100,7 @@ def post_process(df, dates, stn_name, sfx, args):
                 if (fsds_jaws[idx] < toa[idx]*0.05) and (fsds_jaws[idx] != 0):
                     fsds_jaws[idx] = common.fillvalue_float
                     fsus_jaws[idx] = common.fillvalue_float
-                if (fsus_jaws[idx] > fsds_jaws[idx]*0.99) or (fsus_jaws[idx] < fsds_jaws[idx]*0.1):
+                if (fsus_jaws[idx] > fsds_jaws[idx]*0.95) or (fsus_jaws[idx] < fsds_jaws[idx]*0.05):
                     fsus_jaws[idx] = common.fillvalue_float
                 if np.cos(sza[idx]) <= 0:
                     fsds_jaws[idx] = 0
