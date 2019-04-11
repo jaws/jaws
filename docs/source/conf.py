4e09c59a72fcba596b1c738d7704f0a44dbd6300
Ajay Saini <ajcse1@gmail.com>
2019-04-11 13:56:15 -0700

    Add theme_override.css

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -93,7 +93,12 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['nstatic']
+html_static_path = ['_static']
+
+def setup(app):
+    app.add_stylesheet('theme_override.css')
+
+
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
