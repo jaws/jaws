440e0c7cf3beb2c53986bfb59aaa3d7331ae4878
Ajay Saini <ajcse1@gmail.com>
2019-04-20 17:07:05 -0700

    Add metpy as project requirement

--- setup.py
+++ setup.py
@@ -44,7 +44,8 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Operating System :: OS Independent"],
 
-    install_requires=['pandas', 'numpy >= 1.15.2', 'xarray', 'pytz', 'matplotlib', 'netcdf4', 'scipy', 'requests'],
+    install_requires=['pandas', 'numpy >= 1.15.2', 'xarray', 'pytz', 'matplotlib', 'netcdf4', 'scipy', 'requests',
+                      'metpy'],
 
     entry_points = {
         'console_scripts': ['jaws=jaws.jaws:start'],
