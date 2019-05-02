5db84fe44339d2c5fa173cd34fd77022ee674a9c
Ajay Saini <ajcse1@gmail.com>
2019-05-02 16:23:22 -0700

    clearsky.py: renaming, docstring and comments

--- jaws/clearsky.py
+++ jaws/clearsky.py
@@ -12,22 +12,24 @@ except ImportError:
     import common
 
 
-def interpolate(x, y):
-    dv = interp1d(x, y, fill_value='extrapolate')
+def derv_diff(hours, var):
+    """Calculate difference of variable's left and right derivative"""
+    dv = interp1d(hours, var, fill_value='extrapolate')
     alpha = 0.01
-    dv_left, dv_right, tg_left, tg_right = ([None]*len(x) for _ in range(4))
+    dv_left, dv_right, tg_left, tg_right = ([None]*len(hours) for _ in range(4))
     i = 0
-    for hour in x:
+    for hour in hours:
         dv_left[i] = float(dv(i-alpha))
         dv_right[i] = float(dv(i+alpha))
-        tg_left[i] = (y[i] - dv_left[i])/alpha
-        tg_right[i] = (dv_right[i] - y[i])/alpha
+        tg_left[i] = (var[i] - dv_left[i])/alpha
+        tg_right[i] = (dv_right[i] - var[i])/alpha
         i += 1
     diff = [x1 - x2 for (x1, x2) in zip(tg_left, tg_right)]
     return diff
 
 
 def clr_prd(dat_sza, tg_fsds, tg_sza, date, stn_name):
+    """Calculate clear periods (consecutive clear hours)"""
     path = common.relative_path('resources/lst_para_rdn.txt')
     para_file = pd.read_csv(path)
 
@@ -58,12 +60,13 @@ def clr_prd(dat_sza, tg_fsds, tg_sza, date, stn_name):
     for k, g in groupby(enumerate(clr_hrs), lambda ix: ix[0] - ix[1]):
         cons_clr_hrs.append(list(map(itemgetter(1), g)))
     
-    final_hrs, clr_lst = write_to_file(cons_clr_hrs, daylight, date)
+    final_hrs, clr_lst = append_to_list(cons_clr_hrs, daylight, date)
     
     return final_hrs, clr_lst
 
 
 def clr_shift(dat_sza, dat_fill, hrs, date, stn_name):
+    """Recalculate clear periods by adding shift"""
     dat_sza_shift = list(range(len(dat_sza)))
 
     shift = [1, 2, 3]
@@ -77,7 +80,7 @@ def clr_shift(dat_sza, dat_fill, hrs, date, stn_name):
             dat_sza_shift[:i] = [0] * (len(dat_sza_shift) - i)
             dat_sza_shift[i:] = dat_sza[:-i]
 
-        tg_sza_shift = interpolate(hrs,dat_sza_shift)
+        tg_sza_shift = derv_diff(hrs,dat_sza_shift)
 
         cons_clr_hrs, clr_lst = clr_prd(dat_sza, tg_fsds, tg_sza_shift, date, stn_name)
 
@@ -87,7 +90,8 @@ def clr_shift(dat_sza, dat_fill, hrs, date, stn_name):
     return clr_lst
 
 
-def write_to_file(cons_clr_hrs, daylight, date):
+def append_to_list(cons_clr_hrs, daylight, date):
+    """Append clear period to list based on daylight condition"""
     final_hrs = []
 
     if len(daylight) <= 12:
@@ -148,8 +152,8 @@ def main(dataset, args):
         dat_fill = CubicSpline(hours_nonmsng, dat_nonmsng, extrapolate=True)(hrs_30min)
         dat_sza = [np.cos(np.radians(i)) for i in df_temp['sza'].tolist()]
 
-        tg_fsds = interpolate(hrs,dat_fill)
-        tg_sza = interpolate(hrs,dat_sza)
+        tg_fsds = derv_diff(hrs,dat_fill)
+        tg_sza = derv_diff(hrs,dat_sza)
 
         final_hrs, clr_lst = clr_prd(dat_sza, tg_fsds, tg_sza, date, stn_name)
 
